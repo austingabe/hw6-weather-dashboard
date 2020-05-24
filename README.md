adaaa25aff28bc28adc9ac75d3b07e45b3e608b0
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-05-23 23:16:07 -0700

    Appended search history, made comments, and completed README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # hw6-weather-dashboard
-Homework assignment utilizing weather API
+
+A weather application that allows the user to search for a city and its current and future weather conditions. When a search is made, the city name, an icon representing current conditions in that city, and the current temperature, humidity, wind speed, and UV index for that city appear. The UV index will appear green, yellow, or red to indicate good, moderate, and bad conditions, respectively.
+
+In addition to the current weather conditions, a search will also bring up a five-day forecast of that city with dates, icons representing forecasted conditions, future high temperatures, and future humidity levels as well.
+
+A search will append to a list below the search bar each city that has been searched for. Search history is set in local storage, and the latest search query will appear upon page reload. Upon clicking a city in the list, current and future conditions for that city will reappear.
+
+The application will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+The [Moment.js](https://momentjs.com/) library was utilized in generating the date displayed at the top of the page next to the current city as well as each date in the five-day forecast.
+
+Bootstrap (https://getbootstrap.com) was used as the CSS framework for this application.
+
+## Support
+If you have any questions or comments, please contact austinlee721@gmail.com.
+
+## Contributing
+Austin Lee was the only person on this project.
--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+// On DOM load
 $(document).ready(function () {
-
+    // Array that every search submission gets pushed to
     var cityList = [];
 
-
-
+    // Click event
     $("#citysubmit").click(function (e) {
         var city = $("#cityinput").val();
         e.preventDefault();
+        // Pushing every search submission to array
         cityList.push(city);
         console.log(cityList);
 
         var APIKey = "81f64d799bb0f3ddd00a131881aab23e";
-
-        // Here we are building the URL we need to query the database
+        // Building the URL to query the database
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-        // We then created an AJAX call
+        // AJAX call for weather details
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // Log the queryURL
             console.log(queryURL);
-            // Log the resulting object
             console.log(response);
 
             var cityDate = $("#citydate");
             var temperature = $("#temperature");
+            // Converting Kelvin to Fahrenheit
             var calculation = (response.main.temp - 273.15) * 1.80 + 32;
             var fahrenheit = Math.round(calculation * 10) / 10;
             var humidity = $("#humidity");
@@ -43,12 +42,14 @@ $(document).ready(function () {
             var clouds = "Assets/clouds.png";
             var atmosphere = "Assets/atmosphere.png";
 
+            // Date for the top of the page
             cityDate.text(response.name + " " + moment().format("(MMM Do, YYYY)"));
             function ascii(a) { return String.fromCharCode(a); }
             temperature.text("Temperature: " + fahrenheit + ascii(176) + "F");
             humidity.text("Humidity: " + response.main.humidity + "%");
             windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
 
+            // Determining which icons will represent current conditions
             if (main == "Thunderstorm") {
                 currentIcon.attr({
                     src: thunderstorm,
@@ -90,16 +91,19 @@ $(document).ready(function () {
 
             var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey;
 
+            // Calling for more weather info
             $.ajax({
                 url: oneCallURL,
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
+                // Generating dates for forecast
                 var day1Forecast = moment().add(1, "days").format("l");
                 var day2Forecast = moment().add(2, "days").format("l");
                 var day3Forecast = moment().add(3, "days").format("l");
                 var day4Forecast = moment().add(4, "days").format("l");
                 var day5Forecast = moment().add(5, "days").format("l");
+                // Converting forecast Kelvin to Fahrenheit
                 var temp1 = Math.round(((response.daily[1].temp.max - 273.15) * 1.80 + 32) * 10) / 10;
                 var temp2 = Math.round(((response.daily[2].temp.max - 273.15) * 1.80 + 32) * 10) / 10;
                 var temp3 = Math.round(((response.daily[3].temp.max - 273.15) * 1.80 + 32) * 10) / 10;
@@ -115,14 +119,13 @@ $(document).ready(function () {
                 var icon3 = $("#icon3");
                 var icon4 = $("#icon4");
                 var icon5 = $("#icon5");
-                
 
                 $("#date1").text(day1Forecast);
                 $("#date2").text(day2Forecast);
                 $("#date3").text(day3Forecast);
                 $("#date4").text(day4Forecast);
                 $("#date5").text(day5Forecast);
-                    
+
                 $("#temp1").text("Temp: " + temp1 + ascii(176) + "F");
                 $("#temp2").text("Temp: " + temp2 + ascii(176) + "F");
                 $("#temp3").text("Temp: " + temp3 + ascii(176) + "F");
@@ -135,52 +138,51 @@ $(document).ready(function () {
                 $("#hum4").text("Humidity: " + hum4 + "%");
                 $("#hum5").text("Humidity: " + hum5 + "%");
 
-
-                for (var i=1; i < 6; i++) {
+                // Determining icons for forecast cards
+                for (var i = 1; i < 6; i++) {
                     var forecastIcons = [icon1, icon2, icon3, icon4, icon5];
                     var dailyCond = response.daily[i].weather[0].main;
                     console.log(dailyCond);
 
-                if (dailyCond == "Thunderstorm") {
-                    forecastIcons[i-1].attr({
-                        src: thunderstorm,
-                        alt: "Thunderstorm"
-                    });
-                } else if (dailyCond == "Drizzle") {
-                    forecastIcons[i-1].attr({
-                        src: drizzlerain,
-                        alt: "Drizzle"
-                    });
-                } else if (dailyCond == "Rain") {
-                    forecastIcons[i-1].attr({
-                        src: drizzlerain,
-                        alt: "Rain"
-                    });
-                } else if (dailyCond == "Snow") {
-                    forecastIcons[i-1].attr({
-                        src: snow,
-                        alt: "Snow"
-                    });
-                } else if (dailyCond == "Clear") {
-                    forecastIcons[i-1].attr({
-                        src: clear,
-                        alt: "Clear"
-                    });
-                } else if (dailyCond == "Clouds") {
-                    forecastIcons[i-1].attr({
-                        src: clouds,
-                        alt: "Clouds"
-                    });
-                } else {
-                    forecastIcons[i-1].attr({
-                        src: atmosphere,
-                        alt: "Hazy"
-                    })
+                    if (dailyCond == "Thunderstorm") {
+                        forecastIcons[i - 1].attr({
+                            src: thunderstorm,
+                            alt: "Thunderstorm"
+                        });
+                    } else if (dailyCond == "Drizzle") {
+                        forecastIcons[i - 1].attr({
+                            src: drizzlerain,
+                            alt: "Drizzle"
+                        });
+                    } else if (dailyCond == "Rain") {
+                        forecastIcons[i - 1].attr({
+                            src: drizzlerain,
+                            alt: "Rain"
+                        });
+                    } else if (dailyCond == "Snow") {
+                        forecastIcons[i - 1].attr({
+                            src: snow,
+                            alt: "Snow"
+                        });
+                    } else if (dailyCond == "Clear") {
+                        forecastIcons[i - 1].attr({
+                            src: clear,
+                            alt: "Clear"
+                        });
+                    } else if (dailyCond == "Clouds") {
+                        forecastIcons[i - 1].attr({
+                            src: clouds,
+                            alt: "Clouds"
+                        });
+                    } else {
+                        forecastIcons[i - 1].attr({
+                            src: atmosphere,
+                            alt: "Hazy"
+                        })
+                    }
                 }
-                }
-                
 
-
+                // Appending UV index information and determining which color to display
                 var uvNumber = response.current.uvi
                 uvIndex.text("UV Index: " + uvNumber);
                 if (uvNumber < 3) {
@@ -197,22 +199,37 @@ $(document).ready(function () {
                     });
                 }
             })
-
-
-
         });
 
+        var searchHistory = $("ul").attr("id");
+        var citySearch = $(this).siblings("#cityinput").val();
+        // Search history is set to local storage
+        localStorage.setItem(searchHistory, cityList);
+        localStorage.setItem("lastsearched", citySearch);
+        // Array the dynamically created list elements are pushed to in order to append them after each search
+        var showCities = [];
 
-        //     })
+        // Loop to create new list elements and push to array
+        for (var i = 0; i < cityList.length; i++) {
+            var newCity = $("<li>").text(cityList[i]);
+            newCity.attr("class", "list-group-item");
+            showCities.push(newCity);
+            $("#searchhistory").empty();
+        }
 
-        // localStorage.setItem()
-        //     var timeBlock = $(this).parent().attr("id");
-        //     var writtenPlans = $(this).siblings(".plans").val();
-        //     localStorage.setItem(timeBlock, writtenPlans);
-        //   })
+        // Appending search history on each search
+        $("#searchhistory").append(showCities);
 
+
+        // On reload, append the last searched
+        // var lastSearched = localStorage.getItem("lastsearched");
+        // var lastCity = $("<li>").text(lastSearched);
+        // lastCity.attr("class", "list-group-item");
+        // var reloadCity = $("#searchhistory").append(lastCity);
+        // $(window).load(reloadCity);
     });
 
+    // Pressing the enter key functions the same as a click
     $("#cityinput").keypress(function (e) {
         if (e.keyCode === 13) {
             e.preventDefault();
@@ -220,40 +237,9 @@ $(document).ready(function () {
         }
     });
 
-
-
-
+    // Another click event for the side buttons
+    // $(".list-group-item").click(function (e) {
+    //     e.preventDefault();
+    //     $("citysubmit").click();
+    // });
 })
-/* <div id="sixam" class="row time-block">
-      <div class="col-sm-1 hour">6 AM</div>
-
-      <!-- Data-identifier in each timeblock used to compare to the current hour in order to generate dynamically-determined past, present, and future background colors for textarea -->
-
-      <textarea class="description col-sm-10 plans" id="six" data-identifier="6"
-        placeholder="Start Plan Here"></textarea>
-      <button class="col-sm-1 saveBtn"><i class="fas fa-save"></i></button>
-    </div> */
-
-
-//Variables defined in order to compare to current time for dynamically altered textarea background color
-
-// var six = $("#six");
-// var sixData = six.data("identifier");
-
-
-//If, else if statements to append the appropriate class for textarea background color
-// if (sixData < moment().hour()) {
-//     six.addClass("past");
-//   } else if (sixData > moment().hour()) {
-//     six.addClass("future");
-//   } else if (sixData == moment().hour()) {
-//     six.addClass("present");
-//   }
-
-
-
-
-
-  //Value functions getting the values from local storage to show in textarea even upon refresh until the text is altered
-
-// $("#sixam .plans").val(localStorage.getItem("sixam"));
